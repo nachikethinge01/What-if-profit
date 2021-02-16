270d1bf7a70aa05c07b2bce52177db9b6d78b8f4
nachikethinge01 <55408263+nachikethinge01@users.noreply.github.com>
2021-02-15 23:43:24 -0500

    Create WM.php

---
+++ WM.php
@@ -0,0 +1,75 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width-device-width, intial-scale = 1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link href="css-circular-prog-bar.css" rel="stylesheet">
+    <style>
+        body{
+            background-color: #ffc400;
+        }
+
+        .company-title{
+            font-size: 30px;
+            color: black;
+            margin-left: 300px;
+            margin-top: 80px;
+            position: absolute;
+            font-weight: bold;
+        }
+
+        .description{
+            font-size: 25px;
+            color: black;
+            margin-left: 300px;
+            margin-top: 130px;
+            position: absolute;
+        }
+
+        .ranking-1{
+            font-size: 25px;
+            color: black;
+            margin-left: 450px;
+            margin-top: 280px;
+            position: absolute;
+        }
+
+        .ranking-2{
+            font-size: 25px;
+            color: black;
+            margin-left: 850px;
+            margin-top: 280px;
+            position: absolute;
+        }
+
+
+        
+    </style>
+    <title>
+        Project name
+    </title>
+</head>
+
+<body>
+    <div class="company-title">Waste Management</div>
+    <span class="description">Waste Management, Inc. is North America's leading provider of integrated environmental solutions.
+        The Company partners with our customers and communities to manage and reduce waste from
+        collection to disposal while recovering valuable resources and creating clean, renewable energy.
+    </span>
+    
+    <div class="ranking-1">Wall Street Journal:  1st</div>
+    <div class="ranking-2">Forbes:  3rd</div>
+
+ 
+
+
+</body>
+
+</html>
